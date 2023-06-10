6dbeef7b14f9b8d4651458e35975e055b74571d5
Idris Abdulmumin <abumafrim@gmail.com>
2023-06-10 11:34:44 +0100

    Restructuring Codes

--- scripts/get_models_data_repos.sh
+++ scripts/get_models_data_repos.sh
@@ -1,4 +1,19 @@
+# Get hauWE Embeddings
 home_dir=$(pwd)
+
+mkdir embeddings
+cd embeddings
+
+mkdir hausa
+cd hausa
+
+wget https://zenodo.org/record/6997888/files/hauWE-Models.zip
+unzip hauWE-Models.zip
+rm hauWE-Models.zip
+
+cd $home_dir
+
+# Get Panlex-Lexicon-Extractor and PanLex Lite
 
 # make directory Panlex
 mkdir Panlex
@@ -17,5 +32,12 @@ cd data
 wget https://db.panlex.org/panlex_lite.zip
 unzip panlex_lite.zip
 rm panlex_lite.zip
+
+cd $home_dir
+
+# clone or pull Panlex-Lexicon-Extractor
+repo=vecmap
+url=https://github.com/artetxem/vecmap.git
+if cd $repo; then git pull; else git clone $url $repo; fi
 
 cd $home_dir
--- scripts/get_vecmap.sh
+++
@@ -1,4 +0,0 @@
-# clone or pull Panlex-Lexicon-Extractor
-repo=vecmap
-url=https://github.com/artetxem/vecmap.git
-if cd $repo; then git pull; else git clone $url $repo; fi
